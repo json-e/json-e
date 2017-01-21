6903d3e6a4461187bf2e2621788206d983311d39
hammad13060 <hammad13060@iiitd.ac.in>
2017-01-21 15:45:33 +0530

    refactoring for _handleConstructs() done

--- src/index.js
+++ src/index.js
@@ -50,74 +50,86 @@ module.exports = function(_template, _context) {
   /* private */
   function _handleConstructs(template, key) {
     if (template[key]['$if']) {
-      var condition = template[key]['$if'];
-      var hold = undefined;
-      if (typeof condition === 'string' || condition instanceof String) {
-        hold = safeEval(condition, context);
-      } else {
-
-        var err = new Error('invalid construct');
-        err.message = '$if construct must be a string which eval can process';
-        throw err;
-      }
-
-      if (hold) {
-        var hence = template[key]['$then'];
-        if (typeof hence === 'string' || hence instanceof String) {
-          template[key] = _replace(hence);
-        } else if (hence.hasOwnProperty('$eval')) {
-          var dummy = {dummy: template[key]['$then']};
-          _render(dummy);
-          template[key] = dummy['dummy']; 
-        } else {
-          _render(hence);
-          template[key] = hence;
-        }
-      } else {
-        var hence = template[key]['$else'];
-        if (typeof hence === 'string' || hence instanceof String) {
-          template[key] = _replace(hence);
-        } else if (hence.hasOwnProperty('$eval')) {
-          var dummy = {dummy: template[key]['$else']};
-          _render(dummy);
-          template[key] = dummy['dummy']; 
-        } else {
-          _render(hence);
-          template[key] = hence;
-        }
-      }
+      _handleIf(template, key);
     } else if (template[key]['$switch']) {
-      var condition = template[key]['$switch'];
-      var case_option;
-      if (typeof condition === 'string' || condition instanceof String) {
-        case_option = safeEval(condition, context);
-      } else {
-        var err = new Error('invalid construct');
-        err.message = '$switch construct must be a string which eval can process';
-        throw err;
-      }
-      var case_option_value = template[key][case_option];
-      if (typeof case_option_value === 'string' || case_option_value instanceof String) {
-        template[key] = _replace(case_option_value);
-      } else if (case_option_value.hasOwnProperty('$eval')) {
-        var dummy = {dummy: case_option_value};
+      _handleSwitch(template, key);
+    } else if (template[key]['$eval']) {
+      _handleEval(template, key);
+    } else {
+      _render(template[key]);
+    }
+  }
+
+  function _handleIf(template, key) {
+    var condition = template[key]['$if'];
+    var hold = undefined;
+    if (typeof condition === 'string' || condition instanceof String) {
+      hold = safeEval(condition, context);
+    } else {
+
+      var err = new Error('invalid construct');
+      err.message = '$if construct must be a string which eval can process';
+      throw err;
+    }
+
+    if (hold) {
+      var hence = template[key]['$then'];
+      if (typeof hence === 'string' || hence instanceof String) {
+        template[key] = _replace(hence);
+      } else if (hence.hasOwnProperty('$eval')) {
+        var dummy = {dummy: template[key]['$then']};
         _render(dummy);
         template[key] = dummy['dummy']; 
       } else {
-        _render(case_option_value);
-        template[key] = case_option_value;
-      }
-    } else if (template[key]['$eval']) {
-      var expression = template[key]['$eval'];
-      if (typeof expression === 'string' || expression instanceof String) {
-        template[key] = safeEval(expression, context);
-      } else {
-        var err = new Error('invalid constructoruct');
-        err.message = '$eval construct must be a string which eval can process';
-        throw err;
+        _render(hence);
+        template[key] = hence;
       }
     } else {
-      _render(template[key]);
+      var hence = template[key]['$else'];
+      if (typeof hence === 'string' || hence instanceof String) {
+        template[key] = _replace(hence);
+      } else if (hence.hasOwnProperty('$eval')) {
+        var dummy = {dummy: template[key]['$else']};
+        _render(dummy);
+        template[key] = dummy['dummy']; 
+      } else {
+        _render(hence);
+        template[key] = hence;
+      }
+    }
+  }
+
+  function _handleSwitch(template, key) {
+    var condition = template[key]['$switch'];
+    var case_option;
+    if (typeof condition === 'string' || condition instanceof String) {
+      case_option = safeEval(condition, context);
+    } else {
+      var err = new Error('invalid construct');
+      err.message = '$switch construct must be a string which eval can process';
+      throw err;
+    }
+    var case_option_value = template[key][case_option];
+    if (typeof case_option_value === 'string' || case_option_value instanceof String) {
+      template[key] = _replace(case_option_value);
+    } else if (case_option_value.hasOwnProperty('$eval')) {
+      var dummy = {dummy: case_option_value};
+      _render(dummy);
+      template[key] = dummy['dummy']; 
+    } else {
+      _render(case_option_value);
+      template[key] = case_option_value;
+    }
+  }
+
+  function _handleEval(template, key) {
+    var expression = template[key]['$eval'];
+    if (typeof expression === 'string' || expression instanceof String) {
+      template[key] = safeEval(expression, context);
+    } else {
+      var err = new Error('invalid constructoruct');
+      err.message = '$eval construct must be a string which eval can process';
+      throw err;
     }
   }
 
