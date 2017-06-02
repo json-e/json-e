f49ab2ac61a142f9fe0f02d6212d54c4772d9066
Amjad Mashaal <me@amjad.io>
2017-06-02 05:00:17 +0200

    Adding bash header and documentation to test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
-npm test &&
-python setup.py test &&
-npm run-script build-demo
+#!/bin/sh -xe
+
+# This script runs the unit tests for both the JavaScript and the Python
+# implementations of json-e. It also updated the demo website with the new
+# bundle.js file.
+
+npm test && python setup.py test && npm run-script build-demo
