fe8a1587e01992fa1a6526b40b83272dd9156ab8
Amjad Mashaal <me@amjad.io>
2017-06-02 13:48:27 +0200

    Refining test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,15 @@
 # implementations of json-e. It also updated the demo website with the new
 # bundle.js file.
 
-npm test && python setup.py test && npm run-script build-demo
+# JavaScript unit tests
+npm test
+
+# Python unit tests
+python setup.py test
+
+# bundle.js check. Note that the new bundle.js is not deleted
+mv docs/bundle.js docs/bundle.diff.js
+npm run-script build-demo
+diff docs/bundle.js docs/bundle.diff.js
+
+rm docs/bundle.diff.js
