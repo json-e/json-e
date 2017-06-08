bcb828fcbf32d1c67feacb5eaa659a959d89c055
Dustin J. Mitchell <dustin@mozilla.com>
2017-06-08 18:20:01 +0000

    refactor so sub-values get rendered

--- jsone/__init__.py
+++ jsone/__init__.py
@@ -1,9 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import re
-
-from .constructs import constructs
-from .shared import JSONTemplateError, DeleteMarker
+from .render import JSONTemplateError, renderValue
 
 _context_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*$')
 
@@ -11,25 +9,4 @@ _context_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*$')
 def render(template, context):
     if not all(_context_re.match(c) for c in context):
         raise JSONTemplateError('top level keys of context must follow /[a-zA-Z_][a-zA-Z0-9_]*/')
-
-    def recur(template, context):
-        if isinstance(template, basestring):
-            # TODO: interpolate
-            return template
-
-        elif isinstance(template, dict):
-            matches = [k for k in template if k in constructs]
-            if not matches:
-                return template
-            if len(matches) > 1:
-                raise JSONTemplateError("only one construct allowed")
-            return constructs[matches[0]](template, basestring)
-
-        elif isinstance(template, list):
-            rendered = (recur(e, context) for e in template)
-            return [e for e in rendered if e is not DeleteMarker]
-
-        else:
-            return template
-
-    return recur(template, context)
+    return renderValue(template, context)
--- jsone/render.py
+++ jsone/render.py
@@ -1,24 +1,27 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
-import json as jsonmodule
+import re
+import json as json
 from .shared import JSONTemplateError, DeleteMarker
 from . import shared
 
 constructs = {}
 
 
-def construct(fn):
-    constructs['$' + fn.__name__] = fn
-    return fn
+def construct(name):
+    def wrap(fn):
+        constructs[name] = fn
+        return fn
+    return wrap
 
 
-@construct
+@construct('$eval')
 def eval(template, context):
     # TODO: actually evaluate
     return template['$eval']
 
 
-@construct
+@construct('$flatten')
 def flatten(template, context):
     value = template['$flatten']
     if not isinstance(value, list):
@@ -34,7 +37,7 @@ def flatten(template, context):
     return list(gen())
 
 
-@construct
+@construct('$flattenDeep')
 def flattenDeep(template, context):
     value = template['$flattenDeep']
     if not isinstance(value, list):
@@ -51,22 +54,49 @@ def flattenDeep(template, context):
     return list(gen(value))
 
 
-@construct
+@construct('$fromNow')
 def fromNow(template, context):
-    # copied/modified from taskcluster-client.py
-    offset = template['$fromNow']
+    offset = renderValue(template['$fromNow'], context)
     return shared.fromNow(offset)
 
 
-@construct
+@construct('$ifxxx')
+def ifConstruct(template, context):
+    offset = renderValue(template['$if'], context)
+
+
+@construct('$reverse')
 def reverse(template, context):
-    value = template['$reverse']
+    value = renderValue(template['$reverse'], context)
     # TODO: checkType function
     if not isinstance(value, list):
         raise JSONTemplateError("$reverse value must evaluate to an array")
     return list(reversed(value))
 
 
-@construct
-def json(template, context):
-    return jsonmodule.dumps(template['$json'], separators=(',', ':'))
+@construct('$json')
+def jsonConstruct(template, context):
+    value = renderValue(template['$json'], context)
+    return json.dumps(value, separators=(',', ':'))
+
+
+def renderValue(template, context):
+    if isinstance(template, basestring):
+        # TODO: interpolate
+        return template
+
+    elif isinstance(template, dict):
+        matches = [k for k in template if k in constructs]
+        if not matches:
+            return template
+        if len(matches) > 1:
+            raise JSONTemplateError("only one construct allowed")
+        return constructs[matches[0]](template, basestring)
+
+    elif isinstance(template, list):
+        rendered = (renderValue(e, context) for e in template)
+        return [e for e in rendered if e is not DeleteMarker]
+
+    else:
+        # TODO: copy, unquote $$
+        return template
