6caf0b60f59acaf4ce4439fc1b00e1fba090712c
Alexandra da Silva <alesilva241@gmail.com>
2017-09-19 11:58:53 -0300

    first change for  restriction

--- jsone/render.py
+++ jsone/render.py
@@ -124,6 +124,13 @@ def jsonConstruct(template, context):
 @operator('$let')
 def let(template, context):
     variables = renderValue(template['$let'], context)
+    
+    _let_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*$')
+    
+    if type(variables) != list and variables != None:
+        if not all(_let_re.match(key) for key in variables.keys()):
+            raise TemplateError('top level keys of context must follow/[a-zA-Z_][a-zA-Z0-9_]*/')
+    
     if not isinstance(variables, dict):
         raise TemplateError("$let value must evaluate to an object")
     subcontext = context.copy()
