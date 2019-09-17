26118fb43babfabaae3d9c5a100cc1c8c401dda9
Dustin J. Mitchell <dustin@mozilla.com>
2019-09-17 21:03:09 +0000

    some tests for tokenize method

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code)]
 
+use crate::errors::Error;
 use regex;
 use std::collections::HashMap;
 use std::fmt::Write;
-use crate::errors::Error;
 
 struct Tokenizer {
     token_types: Vec<String>,
-    regex: regex::Regex
+    regex: regex::Regex,
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -15,11 +15,15 @@ struct Token {
     token_type: String,
     value: String,
     start: usize,
-    end: usize
+    end: usize,
 }
 
 impl Tokenizer {
-    pub fn new(ignore: String, patterns: HashMap<String, String>, token_types: Vec<String>) -> Tokenizer {
+    pub fn new(
+        ignore: String,
+        patterns: HashMap<String, String>,
+        token_types: Vec<String>,
+    ) -> Tokenizer {
         let re = Tokenizer::build_regex_string(ignore, patterns, &token_types).unwrap();
 
         let regex = regex::Regex::new(&re).unwrap();
@@ -27,7 +31,11 @@ impl Tokenizer {
         Tokenizer { token_types, regex }
     }
 
-    fn build_regex_string(ignore: String, patterns: HashMap<String, String>, token_types: &[String]) -> Result<String, std::fmt::Error> {
+    fn build_regex_string(
+        ignore: String,
+        patterns: HashMap<String, String>,
+        token_types: &[String],
+    ) -> Result<String, std::fmt::Error> {
         let mut re = String::new();
 
         write!(&mut re, "^(?:")?;
@@ -37,7 +45,7 @@ impl Tokenizer {
         for t in token_types {
             match patterns.get(t) {
                 Some(p) => write!(&mut re, "|({})", p)?,
-                None => write!(&mut re, "|({})", regex::escape(t))?
+                None => write!(&mut re, "|({})", regex::escape(t))?,
             }
         }
 
@@ -46,18 +54,18 @@ impl Tokenizer {
         Ok(re)
     }
 
-    pub fn tokenize(self: &mut Self, source: String, offset: usize) -> Vec<Token> {
+    pub fn tokenize(self: &Self, source: String, offset: usize) -> Result<Vec<Token>, Error> {
         let mut last_end = offset;
         let mut tokens = vec![];
 
         loop {
-            let token = self.next(&source, last_end).unwrap();
+            let token = self.next(&source, last_end)?;
             match token {
                 Some(t) => {
                     last_end = t.end;
                     tokens.push(t);
-                },
-                None => return tokens,
+                }
+                None => return Ok(tokens),
             }
         }
     }
@@ -81,39 +89,43 @@ impl Tokenizer {
 
         loop {
             match self.regex.captures(&source[offset..]) {
-                None => if (&source[offset..] != "") {
-                    // no match, but there's input left, so we have an error
-                    return Err(Error::SyntaxError(
-                        format!("unexpected EOF for {} at {}", &source, &source[offset..])
-                    ));
-                } else {
-                    // we've parsed the whole string
-                    return Ok(None);
-                },
+                None => {
+                    if (&source[offset..] != "") {
+                        // no match, but there's input left, so we have an error
+                        return Err(Error::SyntaxError(format!(
+                            "unexpected EOF for {} at {}",
+                            &source,
+                            &source[offset..]
+                        )));
+                    } else {
+                        // we've parsed the whole string
+                        return Ok(None);
+                    }
+                }
                 Some(ref m) => {
                     i = Tokenizer::index_of_not_undefined(m, 1).unwrap();
                     offset += m.get(0).unwrap().end();
 
                     // if it's not an ignored expression, return it
                     if i != 1 {
-                        return Ok(Some(Token{
+                        return Ok(Some(Token {
                             token_type: self.token_types[i - 2].clone(),
                             value: m.get(i).unwrap().as_str().to_string(),
                             start: offset - m.get(0).unwrap().end(),
-                            end: offset
+                            end: offset,
                         }));
                     }
-                },
+                }
             }
         }
     }
 }
 
 mod tests {
+    use crate::errors::Error;
+    use crate::tokenizer::Token;
     use crate::tokenizer::Tokenizer;
     use std::collections::HashMap;
-    use crate::tokenizer::Token;
-    use crate::errors::Error;
 
     fn build_tokenizer() -> Tokenizer {
         let mut hashmap = HashMap::new();
@@ -129,7 +141,7 @@ mod tests {
                 "identifier".to_string(),
                 "+".to_string(),
                 "snowman".to_string(),
-            ]
+            ],
         )
     }
 
@@ -138,8 +150,9 @@ mod tests {
         let re = Tokenizer::build_regex_string(
             "ign".to_string(),
             HashMap::new(),
-            &vec!["abc".to_string(), "def".to_string()]
-        ).unwrap();
+            &vec!["abc".to_string(), "def".to_string()],
+        )
+        .unwrap();
 
         assert_eq!(re, "^(?:(ign)|(abc)|(def))")
     }
@@ -153,8 +166,9 @@ mod tests {
         let re = Tokenizer::build_regex_string(
             "ign".to_string(),
             hashmap,
-            &vec!["number".to_string(), "identifier".to_string()]
-        ).unwrap();
+            &vec!["number".to_string(), "identifier".to_string()],
+        )
+        .unwrap();
 
         assert_eq!(re, "^(?:(ign)|([0-9]+)|([a-z]+))")
     }
@@ -164,19 +178,16 @@ mod tests {
         let re = Tokenizer::build_regex_string(
             "ign".to_string(),
             HashMap::new(),
-            &vec!["*+?".to_string()]
-        ).unwrap();
+            &vec!["*+?".to_string()],
+        )
+        .unwrap();
 
         assert_eq!(re, "^(?:(ign)|(\\*\\+\\?))")
     }
 
     #[test]
     fn constructor_positive() {
-        let t = Tokenizer::new(
-            "ign".to_string(),
-            HashMap::new(),
-            vec!["*+?".to_string()]
-        );
+        let t = Tokenizer::new("ign".to_string(), HashMap::new(), vec!["*+?".to_string()]);
     }
 
     #[test]
@@ -211,12 +222,13 @@ mod tests {
 
         assert_eq!(
             tokenizer.next("abc", 0),
-            Ok(Some(Token{
+            Ok(Some(Token {
                 token_type: "identifier".to_string(),
                 value: "abc".to_string(),
                 start: 0,
                 end: 3
-            })))
+            }))
+        )
     }
 
     #[test]
@@ -225,12 +237,13 @@ mod tests {
 
         assert_eq!(
             tokenizer.next("  abc ", 0),
-            Ok(Some(Token{
+            Ok(Some(Token {
                 token_type: "identifier".to_string(),
                 value: "abc".to_string(),
                 start: 2,
                 end: 5
-            })))
+            }))
+        )
     }
 
     #[test]
@@ -239,12 +252,13 @@ mod tests {
 
         assert_eq!(
             tokenizer.next("  +abc ", 0),
-            Ok(Some(Token{
+            Ok(Some(Token {
                 token_type: "+".to_string(),
                 value: "+".to_string(),
                 start: 2,
                 end: 3
-            })))
+            }))
+        )
     }
 
     #[test]
@@ -253,12 +267,13 @@ mod tests {
 
         assert_eq!(
             tokenizer.next(" 2 +abc ", 0),
-        Ok(Some(Token{
-            token_type: "number".to_string(),
-            value: "2".to_string(),
-            start: 1,
-            end: 2
-        })))
+            Ok(Some(Token {
+                token_type: "number".to_string(),
+                value: "2".to_string(),
+                start: 1,
+                end: 2
+            }))
+        )
     }
 
     #[test]
@@ -267,7 +282,9 @@ mod tests {
 
         assert_eq!(
             tokenizer.next(" * +abc ", 0),
-            Err(Error::SyntaxError("unexpected EOF for  * +abc  at * +abc ".to_string()))
+            Err(Error::SyntaxError(
+                "unexpected EOF for  * +abc  at * +abc ".to_string()
+            ))
         )
     }
 
@@ -275,10 +292,7 @@ mod tests {
     fn next_negative_empty() {
         let tokenizer = build_tokenizer();
 
-        assert_eq!(
-            tokenizer.next("", 0),
-            Ok(None)
-        )
+        assert_eq!(tokenizer.next("", 0), Ok(None))
     }
 
     #[test]
@@ -293,6 +307,68 @@ mod tests {
                 start: 0,
                 end: 3, // snowman is 3 bytes long in utf-8
             }))
+        )
+    }
+
+    #[test]
+    fn tokenize_positive_string_of_tokens_with_whitespace() {
+        let tokenizer = build_tokenizer();
+
+        let result = tokenizer.tokenize("  +☃1234 abdk ☃".to_string(), 0).unwrap();
+        let expected: Vec<Token> = vec![
+            Token {
+                token_type: "+".to_string(),
+                value: "+".to_string(),
+                start: 2,
+                end: 3,
+            },
+            Token {
+                token_type: "snowman".to_string(),
+                value: "☃".to_string(),
+                start: 3,
+                end: 6,
+            },
+            Token {
+                token_type: "number".to_string(),
+                value: "1234".to_string(),
+                start: 6,
+                end: 10,
+            },
+            Token {
+                token_type: "identifier".to_string(),
+                value: "abdk".to_string(),
+                start: 11,
+                end: 15,
+            },
+            Token {
+                token_type: "snowman".to_string(),
+                value: "☃".to_string(),
+                start: 16,
+                end: 19,
+            },
+        ];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn tokenize_negative_empty_string() {
+        let tokenizer = build_tokenizer();
+
+        let result = tokenizer.tokenize("".to_string(), 0).unwrap();
+        let expected: Vec<Token> = vec![];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn tokenize_negative_syntax_error() {
+        let tokenizer = build_tokenizer();
+
+        let result = tokenizer.tokenize("abc !!!!".to_string(), 0);
+        assert_eq!(
+            result,
+            Err(Error::SyntaxError(
+                "unexpected EOF for abc !!!! at !!!!".to_string()
+            ))
         )
     }
 }
