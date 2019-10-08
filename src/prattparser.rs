10b3db2d73aeb24d29b1146b08f86a14ec1fb865
Dustin J. Mitchell <dustin@mozilla.com>
2019-10-08 18:28:24 +0000

    define the prattparser context struct

--- src/prattparser.rs
+++ src/prattparser.rs
@@ -1,12 +1,13 @@
+use crate::errors::Error;
+use crate::tokenizer::Token;
 use crate::tokenizer::Tokenizer;
 use std::collections::HashMap;
-use crate::tokenizer::Token;
 
 pub struct PrattParser<'a> {
     tokenizer: Tokenizer<'a>,
     precedence_map: HashMap<&'a str, usize>,
-    prefix_rules: HashMap<&'a str, fn(&Token) -> usize>,
-    infix_rules: HashMap<&'a str, fn(&Token) -> usize>,
+    prefix_rules: HashMap<&'a str, fn(&Token, &Context) -> usize>,
+    infix_rules: HashMap<&'a str, fn(&Token, &Context) -> usize>,
 }
 
 impl<'a> PrattParser<'a> {
@@ -15,8 +16,8 @@ impl<'a> PrattParser<'a> {
         patterns: HashMap<&str, &str>,
         token_types: Vec<&'a str>,
         precedence: Vec<Vec<&'a str>>,
-        prefix_rules: HashMap<&'a str, fn(&Token) -> usize>,
-        infix_rules: HashMap<&'a str, fn(&Token) -> usize>,
+        prefix_rules: HashMap<&'a str, fn(&Token, &Context) -> usize>,
+        infix_rules: HashMap<&'a str, fn(&Token, &Context) -> usize>,
     ) -> PrattParser<'a> {
         let tokenizer = Tokenizer::new(ignore, patterns, token_types);
         let mut precedence_map: HashMap<&'a str, usize> = HashMap::new();
@@ -43,12 +44,34 @@ impl<'a> PrattParser<'a> {
     }
 }
 
-struct Context {}
+pub struct Context<'a, 'v> {
+    parser: &'a PrattParser<'a>,
+    source: &'v str,
+    context: HashMap<&'a str, &'a str>,
+    next: Result<Option<Token<'a, 'v>>, Error>,
+}
+
+impl<'a, 'v> Context<'a, 'v> {
+    pub fn new(
+        parser: &'a PrattParser,
+        source: &'v str,
+        context: HashMap<&'a str, &'a str>,
+        offset: usize,
+    ) -> Context<'a, 'v> {
+        let next = parser.tokenizer.next(source, offset);
+        Context {
+            source,
+            parser,
+            next,
+            context,
+        }
+    }
+}
 
 mod tests {
-    use crate::prattparser::PrattParser;
-    use std::collections::HashMap;
+    use crate::prattparser::{Context, PrattParser};
     use crate::tokenizer::Token;
+    use std::collections::HashMap;
 
     #[test]
     fn positive_constructor() {
@@ -57,20 +80,20 @@ mod tests {
         patterns.insert("identifier", "[a-z]+");
         patterns.insert("snowman", "☃");
 
-        let mut prefix: HashMap<&str, fn(&Token) -> usize> = HashMap::new();
-        prefix.insert("snowman", |token: &Token|{10});
+        let mut prefix: HashMap<&str, fn(&Token, &Context) -> usize> = HashMap::new();
+        prefix.insert("snowman", |token, context| 10);
 
-        let mut infix: HashMap<&str, fn(&Token) -> usize> = HashMap::new();
-        infix.insert("snowman", |token|{10});
-        infix.insert("+", |token|{10});
+        let mut infix: HashMap<&str, fn(&Token, &Context) -> usize> = HashMap::new();
+        infix.insert("snowman", |token, context| 10);
+        infix.insert("+", |token, context| 10);
 
         let pp = PrattParser::new(
             "[ ]+",
             patterns,
             vec!["number", "identifier", "+", "snowman"],
-            vec![vec!["snowman"],vec!["+"]],
+            vec![vec!["snowman"], vec!["+"]],
             prefix,
-            infix
+            infix,
         );
     }
 }
