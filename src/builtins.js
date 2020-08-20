a65f596a718c93ec3ce898f774e6f2e8ea887039
Noah Laux <noahlaux@gmail.com>
2020-08-20 20:35:40 +0200

    Added js implementation for join feature

--- src/builtins.js
+++ src/builtins.js
@@ -125,6 +125,11 @@ module.exports = (context) => {
     invoke: str => str.replace(/^\s+/, ''),
   });
 
+  define('join', builtins, {
+    argumentTests: ['array', 'string|number'],
+    invoke: (list, separator) => list.join(separator) 
+  });
+
   // Miscellaneous
   define('fromNow', builtins, {
     variadic: 'string',
