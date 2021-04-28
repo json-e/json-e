1a18b9124485251482a5c5832083bc7dbd6d59d8
Dustin J. Mitchell <dustin@mozilla.com>
2021-04-28 21:14:38 +0000

    Give a clear error message when context is not an object
    This was never allowed, but the loose type checking in Python and JS
    would have allowed some non-objects to be passed.

--- py/jsone/__init__.py
+++ py/jsone/__init__.py
@@ -9,6 +9,8 @@ _context_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*$')
 
 
 def render(template, context):
+    if type(context) != dict:
+        raise TemplateError("context must be a dictionary")
     if not all(_context_re.match(c) for c in context):
         raise TemplateError('top level keys of context must follow '
                             '/[a-zA-Z_][a-zA-Z0-9_]*/')
--- py/test/test_misc.py
+++ py/test/test_misc.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import math
 import datetime
-from nose.tools import eq_
+from nose.tools import eq_, assert_raises
 from jsone.shared import string, stringDate
 from jsone import render, JSONTemplateError
 
@@ -11,6 +11,9 @@ def test_custom_builtin():
     def my_builtin(x, y):
         return math.sqrt(x ** 2 + y ** 2)
     eq_(render({'$eval': 'my_builtin(3, 4)'}, {'my_builtin': my_builtin}), 5)
+
+def test_non_object_context():
+    assert_raises(JSONTemplateError, lambda: render({}, "abc"))
 
 def test_same_time_within_evaluation_operator():
     template = [{'$fromNow': ''} for _ in range(1000)]
