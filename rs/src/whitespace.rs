ad19b21bb8a0893de66fd71d1a57092734e35255
Dustin J. Mitchell <dustin@v.igoro.us>
2023-04-16 11:59:35 -0400

    Update comment in rs/src/whitespace.rs
    Co-authored-by: Alex Lopez <alex.lopez.zorzano@gmail.com>

--- rs/src/whitespace.rs
+++ rs/src/whitespace.rs
@@ -1,6 +1,6 @@
 use nom::{character::complete::multispace0, error::ParseError, sequence::delimited, IResult};
 
-/// chomp trailing whitespace
+/// chomp whitespace at either end of a combinator
 // from https://github.com/Geal/nom/blob/master/doc/nom_recipes.md#whitespace
 pub(crate) fn ws<'a, F, O, E: ParseError<&'a str>>(
     inner: F,
