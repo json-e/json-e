2aea0ab515e93eb4998a869b701c75ba51d595ec
Pete Moore <pmoore@mozilla.com>
2026-02-26 19:35:09 +0100

    v4.8.2

--- py/setup.py
+++ py/setup.py
@@ -2,7 +2,7 @@ import json
 import os
 from setuptools import setup, find_packages
 
-version = "4.8.1"
+version = "4.8.2"
 
 description = "A data-structure parameterization system written for embedding context in JSON objects"
 
